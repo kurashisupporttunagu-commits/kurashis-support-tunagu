e043ee89884f9b9bdbb2b964244bb2686c191625
kurashisupporttunagu-commits <kurashisupport.tunagu@gmail.com>
2026-09-24 16:59:15 +0900

    siteフォルダへ完成版を反映

---
+++ site/config.js
@@ -0,0 +1,14 @@
+window.TSUNAGU_CONFIG = Object.freeze({
+  // LINE公式アカウントのURLを入れてください。例: https://lin.ee/xxxxxxx
+  lineUrl: "https://line.me/R/ti/p/@112fuvyy",
+
+  // Netlify Formsを使用します。別サービスへ変更する場合は formProvider を "external" にして、
+  // formEndpointへ送信先を入れてください。
+  formProvider: "netlify",
+
+  // Formspree等を使う場合の送信先。Netlify Forms利用時は空欄のままで構いません。
+  // 例: https://formspree.io/f/xxxxxxxx
+  formEndpoint: "",
+
+  contactEmail: "kurashisupport.tunagu@gmail.com"
+});
